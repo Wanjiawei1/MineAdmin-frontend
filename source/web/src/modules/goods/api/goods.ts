75028451425df6e431be82164011674eff513de3
Wanjiawei <2760404665@qq.com>
2025-08-22 16:56:20 +0800

    feat: add goods management module

--- source/web/src/modules/goods/api/goods.ts
+++ source/web/src/modules/goods/api/goods.ts
@@ -1,4 +1,4 @@
-import { http } from '@/utils/http.ts'
+import request from '@/utils/http.ts'
 
 const prefix = '/goods/goods'
 
@@ -12,25 +12,25 @@ export interface GoodsRecord {
 }
 
 export function page(params: any) {
-  return http.get(`${prefix}/page`, { params })
+  return request.http.get(`${prefix}/page`, { params })
 }
 
 export function getById(id: number) {
-  return http.get(`${prefix}/get`, { params: { id } })
+  return request.http.get(`${prefix}/get`, { params: { id } })
 }
 
 export function add(data: GoodsRecord) {
-  return http.post(`${prefix}/add`, { data })
+  return request.http.post(`${prefix}/add`, { data })
 }
 
 export function edit(data: GoodsRecord) {
-  return http.put(`${prefix}/edit`, { data })
+  return request.http.put(`${prefix}/edit`, { data })
 }
 
 export function deleteByIds(ids: number[]) {
-  return http.delete(`${prefix}/delete`, { data: { ids } })
+  return request.http.delete(`${prefix}/delete`, { data: { ids } })
 }
 
 export function changeStatus(id: number, status: 1 | 2) {
-  return http.put(`${prefix}/changeStatus`, { data: { id, status } })
+  return request.http.put(`${prefix}/changeStatus`, { data: { id, status } })
 }
