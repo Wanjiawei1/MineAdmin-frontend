bd37ef9d9cc3cab17c02b53275f04caef38820bd
Wanjiawei <2760404665@qq.com>
2025-08-25 09:09:48 +0800

    feat: add goods management module

--- source/web/src/modules/goods/views/goods/form.vue
+++ source/web/src/modules/goods/views/goods/form.vue
@@ -17,49 +17,51 @@ const props = defineProps<{
 const goodsForm = ref<MaFormExpose>()
 const goodsModel = ref<Partial<GoodsRecord>>({})
 
-// 定义表单列
-function getFormItems(): MaFormItem[] {
-  return [
-    {
-      formType: 'grid',
-      columns: [
-        {
-          span: 24,
-          formItems: [
-            {
-              label: '商品名称',
-              dataIndex: 'name',
-              formType: 'input',
-              rules: [{ required: true, message: '商品名称不能为空' }],
-            },
-            {
-              label: '商品图片',
-              dataIndex: 'image',
-              formType: 'upload',
-              rules: [{ required: true, message: '请上传商品图片' }],
-            },
-            {
-              label: '商品价格',
-              dataIndex: 'price',
-              formType: 'input-number',
-              rules: [{ required: true, message: '商品价格不能为空' }],
-            },
-            {
-              label: '上架状态',
-              dataIndex: 'status',
-              formType: 'radio-group',
-              initialValue: 1, // 默认值为 "上架"
-              rules: [{ required: true, message: '请选择上架状态' }],
-              options: [
-                { label: '上架', value: 1 },
-                { label: '下架', value: 2 },
-              ],
-            },
-          ],
-        },
-      ],
-    },
-  ]
+// 定义表单列 (返回一个包含 formItems 的对象)
+function getFormItems(): { formItems: MaFormItem[] } {
+  return {
+    formItems: [
+      {
+        formType: 'grid',
+        columns: [
+          {
+            span: 24,
+            formItems: [
+              {
+                label: '商品名称',
+                dataIndex: 'name',
+                formType: 'input',
+                rules: [{ required: true, message: '商品名称不能为空' }],
+              },
+              {
+                label: '商品图片',
+                dataIndex: 'image',
+                formType: 'upload',
+                rules: [{ required: true, message: '请上传商品图片' }],
+              },
+              {
+                label: '商品价格',
+                dataIndex: 'price',
+                formType: 'input-number',
+                rules: [{ required: true, message: '商品价格不能为空' }],
+              },
+              {
+                label: '上架状态',
+                dataIndex: 'status',
+                formType: 'radio-group',
+                initialValue: 1, // 默认值为 "上架"
+                rules: [{ required: true, message: '请选择上架状态' }],
+                options: [
+                  { label: '上架', value: 1 },
+                  { label: '下架', value: 2 },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  }
 }
 
 // 使用 useForm 钩子初始化表单
@@ -67,7 +69,8 @@ useForm('goodsForm').then((form: MaFormExpose) => {
   if (props.formType === 'edit' && props.data) {
     Object.assign(goodsModel.value, props.data)
   }
-  form.setItems(getFormItems())
+  // 正确地传入 getFormItems() 返回对象中的 formItems 数组
+  form.setItems(getFormItems().formItems)
   form.setOptions({
     labelWidth: '80px',
   })
