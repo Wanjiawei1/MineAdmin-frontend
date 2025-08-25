224e3336727baeffd9cc3ccf1e88029c3e0ce8a8
Wanjiawei <2760404665@qq.com>
2025-08-25 09:02:04 +0800

    feat: add goods management module

--- source/web/src/modules/goods/views/goods/form.vue
+++ source/web/src/modules/goods/views/goods/form.vue
@@ -1,72 +1,100 @@
+<!-- 该文件已根据项目规范完全重写 -->
 <script setup lang="ts">
+import { ref } from 'vue'
 import type { GoodsRecord } from '@/modules/goods/api/goods'
-import type { MaFormInstance, MaFormItem } from '@mineadmin/pro-components'
-import { ref, reactive } from 'vue'
-import { add, edit, getById } from '@/modules/goods/api/goods'
+import { add, edit } from '@/modules/goods/api/goods'
+import type { MaFormExpose, MaFormItem } from '@mineadmin/form'
+import useForm from '@/hooks/useForm.ts'
+import { ResultCode } from '@/utils/ResultCode.ts'
+
+defineOptions({ name: 'goods:goods:form' })
 
 const props = defineProps<{
   formType: 'add' | 'edit'
   data?: GoodsRecord
 }>()
-const maForm = ref<MaFormInstance>()
 
-// 表单列定义 (使用 reactive)
-const columns = reactive<MaFormItem[]>([
-  {
-    label: '商品名称',
-    dataIndex: 'name',
-    formType: 'input',
-    rules: [{ required: true, message: '商品名称不能为空' }],
-  },
-  {
-    label: '商品图片',
-    dataIndex: 'image',
-    formType: 'upload',
-    rules: [{ required: true, message: '请上传商品图片' }],
-  },
-  {
-    label: '商品价格',
-    dataIndex: 'price',
-    formType: 'input-number',
-    rules: [{ required: true, message: '商品价格不能为空' }],
-  },
-  {
-    label: '上架状态',
-    dataIndex: 'status',
-    formType: 'radio-group',
-    rules: [{ required: true, message: '请选择上架状态' }],
-    options: [
-      { label: '上架', value: 1 },
-      { label: '下架', value: 2 },
-    ],
-  },
-])
+const goodsForm = ref<MaFormExpose>()
+const goodsModel = ref<Partial<GoodsRecord>>({})
 
-// 初始化
-if (props.formType === 'edit' && props.data?.id) {
-  getById(props.data.id).then((res) => {
-    maForm.value?.setFormModel(res.data)
+// 定义表单列
+function getFormItems(): MaFormItem[] {
+  return [
+    {
+      label: '商品名称',
+      dataIndex: 'name',
+      formType: 'input',
+      rules: [{ required: true, message: '商品名称不能为空' }],
+    },
+    {
+      label: '商品图片',
+      dataIndex: 'image',
+      formType: 'upload',
+      rules: [{ required: true, message: '请上传商品图片' }],
+    },
+    {
+      label: '商品价格',
+      dataIndex: 'price',
+      formType: 'input-number',
+      rules: [{ required: true, message: '商品价格不能为空' }],
+    },
+    {
+      label: '上架状态',
+      dataIndex: 'status',
+      formType: 'radio-group',
+      initialValue: 1, // 默认值为 "上架"
+      rules: [{ required: true, message: '请选择上架状态' }],
+      options: [
+        { label: '上架', value: 1 },
+        { label: '下架', value: 2 },
+      ],
+    },
+  ]
+}
+
+// 使用 useForm 钩子初始化表单
+useForm('goodsForm').then((form: MaFormExpose) => {
+  if (props.formType === 'edit' && props.data) {
+    Object.assign(goodsModel.value, props.data)
+  }
+  form.setItems(getFormItems())
+  form.setOptions({
+    labelWidth: '80px',
+  })
+})
+
+// 创建操作
+function addRequest(): Promise<any> {
+  return new Promise((resolve, reject) => {
+    add(goodsModel.value as GoodsRecord).then((res: any) => {
+      res.code === ResultCode.SUCCESS ? resolve(res) : reject(res)
+    }).catch((err) => {
+      reject(err)
+    })
   })
 }
 
-function addRequest(data: GoodsRecord) {
-  return add(data)
-}
-function editRequest(data: GoodsRecord) {
-  return edit(data)
+// 更新操作
+function editRequest(): Promise<any> {
+  return new Promise((resolve, reject) => {
+    edit(goodsModel.value as GoodsRecord).then((res: any) => {
+      res.code === ResultCode.SUCCESS ? resolve(res) : reject(res)
+    }).catch((err) => {
+      reject(err)
+    })
+  })
 }
 
 defineExpose({
-  add: () => maForm.value?.handleSubmit(addRequest),
-  edit: () => maForm.value?.handleSubmit(editRequest),
-  maForm,
+  add: addRequest,
+  edit: editRequest,
+  maForm: goodsForm,
 })
 </script>
 
 <template>
-  <MaForm
-    ref="maForm"
-    :initial-values="{ status: 1 }"
-    :columns="columns"
-  />
+  <ma-form ref="goodsForm" v-model="goodsModel" />
 </template>
+
+<style scoped lang="scss">
+</style>
