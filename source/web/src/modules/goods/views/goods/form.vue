4b53fa9e78f8ccced89ad04e1a3d52f5cbd97992
Wanjiawei <2760404665@qq.com>
2025-08-25 08:57:24 +0800

    feat: add goods management module

--- source/web/src/modules/goods/views/goods/form.vue
+++ source/web/src/modules/goods/views/goods/form.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import type { GoodsRecord } from '@/modules/goods/api/goods'
-import type { MaFormInstance } from '@mineadmin/pro-components'
-import { ref } from 'vue'
+import type { MaFormInstance, MaFormItem } from '@mineadmin/pro-components'
+import { ref, reactive } from 'vue'
 import { add, edit, getById } from '@/modules/goods/api/goods'
 
 const props = defineProps<{
@@ -9,6 +9,38 @@ const props = defineProps<{
   data?: GoodsRecord
 }>()
 const maForm = ref<MaFormInstance>()
+
+// 表单列定义 (使用 reactive)
+const columns = reactive<MaFormItem[]>([
+  {
+    label: '商品名称',
+    dataIndex: 'name',
+    formType: 'input',
+    rules: [{ required: true, message: '商品名称不能为空' }],
+  },
+  {
+    label: '商品图片',
+    dataIndex: 'image',
+    formType: 'upload',
+    rules: [{ required: true, message: '请上传商品图片' }],
+  },
+  {
+    label: '商品价格',
+    dataIndex: 'price',
+    formType: 'input-number',
+    rules: [{ required: true, message: '商品价格不能为空' }],
+  },
+  {
+    label: '上架状态',
+    dataIndex: 'status',
+    formType: 'radio-group',
+    rules: [{ required: true, message: '请选择上架状态' }],
+    options: [
+      { label: '上架', value: 1 },
+      { label: '下架', value: 2 },
+    ],
+  },
+])
 
 // 初始化
 if (props.formType === 'edit' && props.data?.id) {
@@ -35,35 +67,6 @@ defineExpose({
   <MaForm
     ref="maForm"
     :initial-values="{ status: 1 }"
-    :columns="[
-      {
-        label: '商品名称',
-        dataIndex: 'name',
-        formType: 'input',
-        rules: [{ required: true, message: '商品名称不能为空' }],
-      },
-      {
-        label: '商品图片',
-        dataIndex: 'image',
-        formType: 'upload',
-        rules: [{ required: true, message: '请上传商品图片' }],
-      },
-      {
-        label: '商品价格',
-        dataIndex: 'price',
-        formType: 'input-number',
-        rules: [{ required: true, message: '商品价格不能为空' }],
-      },
-      {
-        label: '上架状态',
-        dataIndex: 'status',
-        formType: 'radio-group',
-        rules: [{ required: true, message: '请选择上架状态' }],
-        options: [
-          { label: '上架', value: 1 },
-          { label: '下架', value: 2 },
-        ],
-      },
-    ]"
+    :columns="columns"
   />
 </template>
