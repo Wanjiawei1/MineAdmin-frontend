6876445983e0699b8dcd7514872ec89ecbf8ba60
Wanjiawei <2760404665@qq.com>
2025-08-25 09:43:42 +0800

    feat: add goods management module

--- source/web/src/modules/goods/views/goods/data/getFormItems.tsx
+++ source/web/src/modules/goods/views/goods/data/getFormItems.tsx
@@ -1,4 +1,6 @@
 import type { MaFormItem } from '@mineadmin/form'
+import MaUploadImage from '@/components/ma-upload-image/index.vue'
+import MaDictRadio from '@/components/ma-dict-picker/ma-dict-radio.vue'
 
 export default function getFormItems(): { formItems: MaFormItem[] } {
   return {
@@ -11,32 +13,43 @@ export default function getFormItems(): { formItems: MaFormItem[] } {
             formItems: [
               {
                 label: '商品名称',
-                dataIndex: 'name',
-                formType: 'input',
-                rules: [{ required: true, message: '商品名称不能为空' }],
+                prop: 'name', // 修正: dataIndex -> prop
+                render: 'input', // 修正: formType -> render
+                itemProps: { // 修正: rules 包装在 itemProps 中
+                  rules: [{ required: true, message: '商品名称不能为空' }],
+                },
               },
               {
                 label: '商品图片',
-                dataIndex: 'image',
-                formType: 'upload',
-                rules: [{ required: true, message: '请上传商品图片' }],
+                prop: 'image', // 修正: dataIndex -> prop
+                render: () => MaUploadImage, // 修正: formType -> render
+                itemProps: { // 修正: rules 包装在 itemProps 中
+                  rules: [{ required: true, message: '请上传商品图片' }],
+                },
               },
               {
                 label: '商品价格',
-                dataIndex: 'price',
-                formType: 'input-number',
-                rules: [{ required: true, message: '商品价格不能为空' }],
+                prop: 'price', // 修正: dataIndex -> prop
+                render: 'input-number', // 修正: formType -> render
+                itemProps: { // 修正: rules 包装在 itemProps 中
+                  rules: [{ required: true, message: '商品价格不能为空' }],
+                },
               },
               {
                 label: '上架状态',
-                dataIndex: 'status',
-                formType: 'radio-group',
-                initialValue: 1, // 默认值为 "上架"
-                rules: [{ required: true, message: '请选择上架状态' }],
-                options: [
-                  { label: '上架', value: 1 },
-                  { label: '下架', value: 2 },
-                ],
+                prop: 'status', // 修正: dataIndex -> prop
+                render: () => MaDictRadio, // 修正: formType -> render
+                renderProps: { // 修正：使用 renderProps 传递字典和选项
+                  dictName: 'system-status', // 假设存在一个名为 'system-status' 的字典
+                  // 或者直接提供 options
+                  // options: [
+                  //   { label: '上架', value: 1 },
+                  //   { label: '下架', value: 2 },
+                  // ],
+                },
+                itemProps: { // 修正: rules 包装在 itemProps 中
+                  rules: [{ required: true, message: '请选择上架状态' }],
+                },
               },
             ],
           },
